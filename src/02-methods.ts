8388491148c4659fc0b3711dfb29970c3834bc96
Danniel Navas <dannielnavas@gmail.com>
2023-03-02 06:02:13 -0500

    ✨ Methods

---
+++ src/02-methods.ts
@@ -0,0 +1,36 @@
+export class MyDate {
+  // TODO: typescript solicita que siempre se inicialien las variables
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  printFormat(): string {
+    return `${this.year}-${this.month}-${this.day}`;
+  }
+  add(amount: number, type: 'days' | 'mounths' | 'years'): void {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'mounths') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+}
+
+const myDate = new MyDate(2023, 2, 2);
+console.log(myDate.printFormat());
+
+myDate.add(1, 'days');
+console.log(myDate.printFormat());
+
+myDate.add(1, 'mounths');
+console.log(myDate.printFormat());
