74386907e6e94606e18604872ff02168f7ee2fe4
Danniel Navas <dannielnavas@gmail.com>
2023-03-02 06:06:57 -0500

    ✨ Private access

---
+++ src/04-private.ts
@@ -0,0 +1,59 @@
+export class MyDate {
+  // TODO: typescript solicita que siempre se inicialien las variables
+  private year: number;
+  private month: number;
+  // FIXED: readonly day: number; da error para poder modificar internamente
+  private day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  printFormat(): string {
+    return `${this.year}-${this.month}-${this.day}`;
+  }
+  add(amount: number, type: 'days' | 'mounths' | 'years'): void {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'mounths') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+  getDay(): number {
+    return this.day;
+  }
+  getMonth(): number {
+    return this.month;
+  }
+  getYear(): number {
+    return this.year;
+  }
+
+  setDay(day: number): void {
+    this.day = day;
+  }
+  setMonth(month: number): void {
+    this.month = month;
+  }
+  setYear(year: number): void {
+    this.year = year;
+  }
+}
+
+const myDate = new MyDate(2023, 2, 2);
+console.log(myDate.printFormat());
+
+myDate.add(1, 'days');
+console.log(myDate.printFormat());
+
+myDate.add(1, 'mounths');
+console.log(myDate.printFormat());
+
+// TODO: es pelñigroso asignar desde afuera una variable de una clase
+// myDate.day = 12
