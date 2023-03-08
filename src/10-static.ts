cae14d660cab0b5d34d0f845446daef86c9edc31
Danniel Navas <dannielnavas@gmail.com>
2023-03-08 08:06:34 -0500

    ✨ static

---
+++ src/10-static.ts
@@ -0,0 +1,14 @@
+console.log(Math.PI);
+console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+export class MyMath {
+  // TODO: es estatica y de solo lectura
+  static readonly PI: number = 3.1416;
+
+  static max(...numbers: number[]): number {
+    return numbers.reduce((max, current) => (current > max ? current : max));
+  }
+}
+
+console.log(MyMath.PI);
+console.log(MyMath.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
