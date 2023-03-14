4d4fc261bf9223983c78ea00a6be8d6de60678fa
Danniel Navas <dannielnavas@gmail.com>
2023-03-14 07:11:06 -0500

    📝 generics

--- src/app/services/product-crud.service.ts
+++ src/app/services/product-crud.service.ts
@@ -2,7 +2,7 @@ import { UpdateProductDto } from '../dtos/product.dto';
 import { Product } from '../product.model';
 // import { BaseHttpService } from './base-http.service';
 import { ProductHttpService } from './product-http2.service';
-
+//TODO: los genericos es mayormente para reutilizar codigo de librerias y no de productos
 export class ProductCrudService {
   private endpoint = 'https://api.escuelajs.co/api/v1/products';
   // private http = new BaseHttpService<Product>(this.endpoint);
